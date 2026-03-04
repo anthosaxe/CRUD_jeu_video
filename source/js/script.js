934e2e0132d0d52c57968bedfa45c4a2ddbf8b7f
Antho D <dhaene.anthony@hotmail.be>
2026-03-04 19:44:46 +0100

    maj boutton

--- source/js/script.js
+++ source/js/script.js
@@ -1,3 +1,9 @@
 document.addEventListener('DOMContentLoaded', function () {
-    new DataTable('#jv_table');
+    new DataTable('#jv_table', {
+        language: {
+            lengthMenu: "Jeux affichés : _MENU_",
+            info: "Affichage de _START_ à _END_ sur _TOTAL_ jeux",
+            search: "Rechercher :"
+        }
+    });
 });
